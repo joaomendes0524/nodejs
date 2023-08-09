81d2738753e6cd54e7df283beaab1efd5cc9266b
João Vitor Mendes <124755170+June052400@users.noreply.github.com>
2023-08-08 22:23:31 -0300

    Add files via upload
    nesta aula usei o pacote "express" do nodejs, pacotes podem ser instalados no site NPM.
    instalei postman para fazer requisição de informações do servidor, que criei e inicializei no powershell.

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-/*DECLARANDO VARIÁVEIS*/
-var nome;
-nome = 'JOAO VITOR MENDES COSTA';
-let idade = 16;
-const linguagem = 'JS';
+//importando um módulo do express:
+const express = require('express');
 
-console.log("ETEC DE BARUERI - PW2 - NODEJS")
-console.log('NOME: '+ nome);
-console.log('IDADE: '+ idade)
-console.log('LINGUAGEM: ' + linguagem);
+//cria uma instancia executável do express:
+const app = express();
+//rota de get(RAIZ)
+app.get('/',(req, res)=>{
+    res.send('EXEMPLO DE ROTA DE GET');
+});
+//rota de get clientes(RAIZ)
+app.get('/clientes',(req, res)=>{
+    res.send('ROTA GET DE CLIENTES!!!');
+});
+
+//rota de cadastro (POST)
+app.post('/cadastrar', (req, res)=>{
+    res.send('ROTA POST DE CADASTRO');
+});
+
+//rota de alteração (PUT)
+app.put('/alterar', (req, res)=>{
+    res.send('ROTA PUT DE ALTERAÇÃO');
+});
+
+//rota de excluir (DELETE)
+app.delete('/excluir', (req, res)=>{
+    res.send('ROTA DE EXCLUSÃO');
+});
+
+//servidor requisição/resposta
+app.listen(3000, ()=>{
+    console.log('SERVIDOR RODANDO!')
+});
